64e3fdc112f44a1f80e1161f8a2bb3f1e63c528b
Amandine <amandine.arnaudy11@gmail.com>
2023-11-13 17:09:50 +0100

    Log Out

--- argentbank-react/src/containers/headerUser.jsx
+++ argentbank-react/src/containers/headerUser.jsx
@@ -1,27 +1,44 @@
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import argentBankLogo from '../assets/argentBankLogo.png';
 import '../styles/index.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-function UserHeader () {
+export const HeaderUser = () => {
+    const dispatch = useDispatch();
+    const userName = useSelector((state) => state.user.user.userName);
+    const navigate = useNavigate();
 
-        return (
-            <nav className="main-nav">
-                <Link to="/" className="main-nav-logo">
-                    <img
-                        className="main-nav-logo-image"
-                        src={argentBankLogo}
-                        alt="Argent Bank Logo"
-                    />
-                    <h1 className="sr-only">Argent Bank</h1>
+    const clearToken = (e) => {
+        e.preventDefault();
+        dispatch({ type: 'LOGOUT' });
+        navigate('/sign-in');
+    };
+
+    return (
+        <nav className="main-nav">
+            <Link className="main-nav-logo" to="/">
+                <img
+                    className="main-nav-logo-image"
+                    src={argentBankLogo}
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+            <div>
+                <Link className="main-nav-item" to="/user">
+                    {userName}
+                    <i className="fa fa-user-circle"></i>
                 </Link>
-                <div>
-                    <Link to="./sign-in" className="main-nav-item">
-                        <i className="fa fa-user-circle"></i>
-                        Sign Out
-                    </Link>
-                </div>
-            </nav>
-        );
-    }
+                <Link className="main-nav-item">
+                    <i className="fa-solid fa-gear"></i>
+                </Link>
+                <Link className="main-nav-item" onClick={clearToken}>
+                    <i className="fa-solid fa-power-off"></i>
+                </Link>
+            </div>
+        </nav>
+    );
+};
 
-export default UserHeader;
+export default HeaderUser;
